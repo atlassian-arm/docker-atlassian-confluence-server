18f7a97aadd081cea5a6b217e07ac63550880385
Adam Brokes <abrokes@atlassian.com>
2022-03-29 21:54:33 +1100

    DCD-1479: Change the base image

--- pipelines-generator.py
+++ pipelines-generator.py
@@ -12,8 +12,8 @@ images = {
             'start_version': '7.4',
             'end_version': '8',
             'default_release': True,
-            'base_image': 'adoptopenjdk/openjdk11',
-            'tag_suffixes': ['adoptopenjdk11', 'jdk11', 'ubuntu', 'ubuntu-18.04-adoptopenjdk11'],
+            'base_image': 'eclipse-temurin:11',
+            'tag_suffixes': ['adoptopenjdk11', 'jdk11', 'ubuntu'],
             'dockerfile': 'Dockerfile',
             'docker_repos': REPOS,
         }
